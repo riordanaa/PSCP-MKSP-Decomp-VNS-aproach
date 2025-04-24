9d60fb0044335536dd54873ef52d2008f5892299
Raúl Martín <raul.martin@urjc.es>
2025-04-24 12:59:02 +0200

    improve model

--- src/main/java/es/urjc/etsii/grafo/PSSC/model/PSSCInstance.java
+++ src/main/java/es/urjc/etsii/grafo/PSSC/model/PSSCInstance.java
@@ -5,18 +5,52 @@ import es.urjc.etsii.grafo.util.collections.BitSet;
 
 public class PSSCInstance extends Instance {
 
-    private final BitSet[] coverage;
+    /**
+     * Number of sets in the instance
+     */
     private final int nSets;
+
+    /**
+     * Number of points in the instance
+     */
     private final int nPoints;
+
+    /**
+     * For each set, which points does it cover
+     */
+    private final BitSet[] coverage;
+
+    /**
+     * Which sets should not be added to the solution
+     * because they either do not cover any point,
+     * or there is always a better set that includes them.
+     */
+    private final BitSet uselessSets;
+
 
     public PSSCInstance(int nSets, int nPoints, BitSet[] coverage, String name){
         super(name);
         this.nSets = nSets;
         this.nPoints = nPoints;
         this.coverage = coverage;
+        this.uselessSets = new BitSet(nSets);
+
+        for (int i = 0; i < nSets; i++) {
+            var set = coverage[i];
+            if (set.isEmpty()) {
+                uselessSets.add(i);
+            } else {
+                for (int j = 0; j < nSets; j++) {
+                    if(i != j && set.containsAll(coverage[j])) {
+                        uselessSets.add(j);
+                    }
+                }
+            }
+        }
 
         setProperty("nSets", nSets);
         setProperty("nPoints", nPoints);
+        setProperty("nUselessSets", uselessSets.size());
     }
 
 
@@ -32,19 +66,45 @@ public class PSSCInstance extends Instance {
         return Integer.compare(this.nSets, otherInstance.nSets);
     }
 
+    /**
+     * Returns the set of points covered by each sets
+     * @return set of points covered by each sets
+     */
     public BitSet[] getCoverage() {
         return coverage;
     }
 
+    /**
+     * Returns the number of sets in the instance
+     * @return number of sets
+     */
     public int getnSets() {
         return nSets;
     }
 
+    /**
+     * Returns the number of points in the instance
+     * @return number of points
+     */
     public int getnPoints() {
         return nPoints;
     }
 
+    /**
+     * Returns the set of points covered by a set
+     * @param set set id, 0 indexed.
+     * @return set of points covered by the set
+     */
     public BitSet getCoveredPoints(int set) {
         return coverage[set];
+    }
+
+    /**
+     * Returns a collection of sets that should not be added to the solution,
+     * as there will always be better options.
+     * @return collection of useless sets, as a BitSet
+     */
+    public BitSet getUselessSets() {
+        return uselessSets;
     }
 }
--- src/main/java/es/urjc/etsii/grafo/PSSC/model/PSSCSolution.java
+++ src/main/java/es/urjc/etsii/grafo/PSSC/model/PSSCSolution.java
@@ -1,52 +1,125 @@
 package es.urjc.etsii.grafo.PSSC.model;
 
 import es.urjc.etsii.grafo.solution.Solution;
-import es.urjc.etsii.grafo.util.DoubleComparator;
 import es.urjc.etsii.grafo.util.collections.BitSet;
 
 public class PSSCSolution extends Solution<PSSCSolution, PSSCInstance> {
 
+    /**
+     * Minimum % coverage required to be considered feasible.
+     */
     public static final double MIN_COVERAGE = 0.9;
 
+    /**
+     * Minimum number of points that must be covered to be considered feasible.
+     */
+    final int minCoveredRequired;
+
+    /**
+     * Set of sets chosen whose points will be covered
+     */
     BitSet chosenSets;
+
+    /**
+     * Number of times each point is covered by each set. An uncovered point value is 0.
+     */
+    int[] coverCount;
 
     public PSSCSolution(PSSCInstance instance) {
         super(instance);
         this.chosenSets = new BitSet(instance.getnSets());
+        this.minCoveredRequired = (int) Math.ceil(instance.getnPoints() * MIN_COVERAGE);
+        this.coverCount = new int[instance.getnPoints()];
     }
 
     public PSSCSolution(PSSCSolution solution) {
         super(solution);
         this.chosenSets = solution.chosenSets.clone();
+        this.minCoveredRequired = solution.minCoveredRequired;
+        this.coverCount = solution.coverCount.clone();
     }
 
+    /**
+     * Returns the number of sets chosen in this solution. We must minimize the number of sets chosen.
+     * @return number of sets chosen
+     */
     public int getScore() {
         return chosenSets.size();
     }
 
+    /**
+     *
+     * @return true if the minimum coverage is reached
+     */
     public boolean isCovered() {
-        return DoubleComparator.isGreaterOrEquals(coverage(), MIN_COVERAGE);
+        return coveredPoints().size() >= minCoveredRequired;
     }
 
-    public double coverage() {
+    /**
+     * Compute all covered and uncovered points
+     * @return set that contains all points covered by the chosen sets
+     */
+    public BitSet coveredPoints() {
         var instance = getInstance();
         BitSet coveredPoints = new BitSet(instance.getnPoints());
         for (int set : this.chosenSets) {
             coveredPoints.or(instance.getCoveredPoints(set));
         }
-        return coveredPoints.size() / (double) instance.getnPoints();
+        return coveredPoints;
     }
 
-    public void addSet(int set) {
+    public double coverage() {
+        return coveredPoints().size() / (double) minCoveredRequired;
+    }
+
+    /**
+     * Picks a set, all its points will now be covered.
+     * @param set set to add to the solution
+     * @return number of new points covered by this set
+     */
+    public int addSet(int set) {
         chosenSets.add(set);
+        int newCovered = 0;
+        for(var point : getInstance().getCoveredPoints(set)){
+            if(coverCount[point] == 0){
+                newCovered++;
+            }
+            coverCount[point]++;
+        }
+        return newCovered;
     }
 
-    public void removeSet(int set) {
+    /**
+     * Removes a set, its points may or may not be covered by other sets.
+     * @param set set to remove from the solution
+     * @return number of points that are no longer covered after removing this set
+     */
+    public int removeSet(int set) {
         chosenSets.remove(set);
+        int newUncovered = 0;
+        for(var point : getInstance().getCoveredPoints(set)){
+            if(coverCount[point] == 1){
+                newUncovered++;
+            }
+            coverCount[point]--;
+        }
+        return newUncovered;
     }
 
+    /**
+     * Returns all currently chosen sets
+     * @return set of sets chosen in this solution.
+     */
     public BitSet getChosenSets() {
         return chosenSets;
+    }
+
+    /**
+     * Minimum points that must be covered for the solution to be feasible
+     * @return
+     */
+    public int minCoveredRequired(){
+        return minCoveredRequired;
     }
 
     @Override
