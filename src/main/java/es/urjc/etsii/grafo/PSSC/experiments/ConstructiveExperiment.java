6394f067ac12d9231c11775e407759e94cfffcdf
Aidan Riordan <89556729+riordanaa@users.noreply.github.com>
2025-06-27 23:29:06 -0400

    Version for swapdrop only
    go to previous version for drop and swap neighborhood vns seperately

--- src/main/java/es/urjc/etsii/grafo/PSSC/experiments/ConstructiveExperiment.java
+++ src/main/java/es/urjc/etsii/grafo/PSSC/experiments/ConstructiveExperiment.java
@@ -1,14 +1,12 @@
 package es.urjc.etsii.grafo.PSSC.experiments;
 
-// --- ADD THIS ENTIRE BLOCK ---
 import es.urjc.etsii.grafo.PSSC.algorithms.PSSCVariableNeighborhoodDescent;
 import es.urjc.etsii.grafo.PSSC.constructives.PSSCGreedyConstructive;
-import es.urjc.etsii.grafo.PSSC.constructives.PSSCRandomConstructive;
 import es.urjc.etsii.grafo.PSSC.model.PSSCInstance;
 import es.urjc.etsii.grafo.PSSC.model.PSSCSolution;
 import es.urjc.etsii.grafo.PSSC.model.neigh.DropNeighborhood;
-import es.urjc.etsii.grafo.PSSC.model.neigh.SwapNeighborhood;
-import es.urjc.etsii.grafo.PSSC.shake.PSSCAddShake;
+import es.urjc.etsii.grafo.PSSC.model.neigh.SwapAndDropNeighborhood;
+import es.urjc.etsii.grafo.PSSC.shake.ShakeDestroyAndRepair;
 import es.urjc.etsii.grafo.algorithms.Algorithm;
 import es.urjc.etsii.grafo.algorithms.SimpleAlgorithm;
 import es.urjc.etsii.grafo.experiment.AbstractExperiment;
@@ -18,34 +16,56 @@ import es.urjc.etsii.grafo.improve.ls.LocalSearchBestImprovement;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Experiment that benchmarks different algorithms among them.
+ * This file assembles the components into runnable algorithms.
+ */
 public class ConstructiveExperiment extends AbstractExperiment<PSSCSolution, PSSCInstance> {
 
+    /**
+     * Defines the list of algorithms to be executed in this experiment.
+     *
+     * @return A list containing the configured algorithms.
+     */
     @Override
     public List<Algorithm<PSSCSolution, PSSCInstance>> getAlgorithms() {
+
         var algorithms = new ArrayList<Algorithm<PSSCSolution, PSSCInstance>>();
-        algorithms.add(new SimpleAlgorithm<>("Random", new PSSCRandomConstructive()));
 
-        // --- ASSEMBLE OUR VNS ALGORITHM ---
+        // ALGORITHM 1: A fast, greedy constructive algorithm to serve as a baseline.
+        algorithms.add(new SimpleAlgorithm<>("Greedy", new PSSCGreedyConstructive()));
+
+        // --- ASSEMBLE OUR VNS ALGORITHM (ALGORITHM 2) ---
+
+        // 1. Define the constructive heuristic for the VNS to start with.
         var constructive = new PSSCGreedyConstructive();
-        algorithms.add(new SimpleAlgorithm<>("Greedy", constructive));
-        Improver<PSSCSolution, PSSCInstance> dropImprover = new LocalSearchBestImprovement<>(new DropNeighborhood());
-        Improver<PSSCSolution, PSSCInstance> swapImprover = new LocalSearchBestImprovement<>(new SwapNeighborhood());
-        var shake = new PSSCAddShake();
 
-        // Define the maximum shake strength for our VNS
-        int kMax = 20; // Example value, can be tuned
+        // 2. Define the improvers for our manual VND loop.
+        var dropImprover = new LocalSearchBestImprovement<>(new DropNeighborhood());
+        
+        // Instantiate the new, OPTIMIZED SwapAndDrop neighborhood.
+        // 50 is a good starting candidate list size to test.
+        var swapAndDropImprover = new LocalSearchBestImprovement<>(new SwapAndDropNeighborhood(50));
 
+        // 3. Instantiate our "Destroy and Repair" Shake component.
+        var shake = new ShakeDestroyAndRepair();
+
+        // 4. Define the maximum shake strength.
+        int kMax = 20;
+
+        // 5. Create an instance of our custom VNS algorithm.
         var myVNS = new PSSCVariableNeighborhoodDescent(
-                "MyVNS_with_Shake",
+                "MyVNS_OptimizedSwapDrop", // New name to reflect the change
                 constructive,
                 dropImprover,
-                swapImprover,
+                swapAndDropImprover, // Pass the new, optimized improver
                 shake,
-                kMax // Pass kMax to the constructor
+                kMax
         );
 
+        // 6. Add the fully assembled VNS to the experiment list.
         algorithms.add(myVNS);
+
         return algorithms;
     }
 }
